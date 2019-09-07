8088dd2a2f6744a30614f5af3d9188e065f2e728
Elias <eeliasmaia@gmail.com>
2019-09-07 14:59:18 -0300

    Create directory controllers, create a separated file for routes, create the first controller - ProductController

--- server.js
+++ server.js
@@ -9,18 +9,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true});
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-
-//Criando a primeira rota
-app.get('/', (req, res) =>{
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  });
-
-  return res.send("Hello Rocketseat");
-});
+//Rotas 
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
